081998c0af22811cbe6c19073cce8ff510f15c20
Ebubekir <ebubekirn96@gmail.com>
2023-01-23 16:47:12 +0300

    Add project files.

---
+++ JwtOrnek/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtOrnek.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize] // Bunu sayesinde sadece authorize olan kullanıcılar bunu görebilir.
+	public class AuthController : ControllerBase
+	{
+		string signingKey = "BuBenimSigningKeyim"; // Bu anahtar ile şifreleme çözülür.
+
+		[HttpGet]
+		public string Get(string userName, string password)
+		{
+			// claims => payload
+			var claims = new[]
+			{
+				new Claim(ClaimTypes.Name, userName),
+				new Claim(JwtRegisteredClaimNames.Email, userName)
+			};
+
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey)); 
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+			var jwtSecurityToken = new JwtSecurityToken(
+				issuer: "https://www.abcd.com",
+				audience: "BenimAudienceDegerim",
+				claims: claims,
+				expires: DateTime.Now.AddDays(15),
+				notBefore: DateTime.Now,
+				signingCredentials: credentials
+			);
+
+			var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+			return token;
+		}
+
+		[HttpGet("ValidateToken")]
+		public bool ValidateToken(string token)
+		{
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey));
+			try
+			{
+				JwtSecurityTokenHandler handler = new();
+				handler.ValidateToken(token, new TokenValidationParameters()
+				{
+					ValidateIssuerSigningKey= true,
+					IssuerSigningKey = securityKey,
+					ValidateLifetime = true,
+					ValidateAudience = false,
+					ValidateIssuer = false
+				}, out SecurityToken validateToken);
+				var jwtToken = (JwtSecurityToken)validateToken;
+
+				return true;var claims = jwtToken.Claims.ToList();
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ JwtOrnek/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var issuer = builder.Configuration["JwtConfig:Issuer"];
+var audience = builder.Configuration["JwtConfig:Audience"];
+var signingKey = builder.Configuration["JwtConfig:SigningKey"];
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = issuer,
+		ValidAudience = audience,
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey))
+	};
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Önce UseAuthentication sonra UseAuthorization olmasý gerekir.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
